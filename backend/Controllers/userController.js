43f058915e94b624b9550ca8a9f62b12f307a40d
Sudhais <87361938+sudhais@users.noreply.github.com>
2024-05-07 02:31:06 +0530

    added coockies in backend

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,29 +1,33 @@
 import userModel from '../models/User.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
-export const login = (req, res) => {
+export const login = (req, res,next) => {
   const { email, password } = req.body;
 
   userModel.findOne({email:email})
     .then((user) => {
-      if (user) {
-        if (user.password === password) {
-          res.status(200).json({
-            success:true,
-            user
-          })
-        } else {
-          res.status(401).json('the password is incorrect')
-        }
-      } else {
-        res.status(401).json('No Record exists')
-      }
+
+      if (!user) {
+        return next(errorHandler(404,'User not found'));       
+      } 
+
+      const validPassword = bcryptjs.compareSync(password,user.password);
+      if (!validPassword) {
+        // res.status(401).json('the password is incorrect')
+        return next(errorHandler(401,'Invalid Credentials'));       
+      } 
+      const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+      const {password:hashedPassword,...rest} = user._doc
+      const expiryDate = new Date(Date.now() + (1000 * 60 * 60)) //1 hour
+      res.cookie('access_token', token, {httpOnly: true, expires: expiryDate}).status(200).json({
+        success:true,
+        user:rest
+      });
     })
     .catch((error) => {
-      res.status(500).json({
-          message:'Internal Server Error',
-          error: error.message
-        })
+      next(error);
     })
 
 };
